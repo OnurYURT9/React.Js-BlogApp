7615c1756a6590ec279fda16ca1f39569cfd6745
ONUR YURT <oyurt43@gmail.com>
2021-08-08 03:46:38 +0300

    ilk

---
+++ src/firebase/firebaseconfig.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApAJaxLK918QC3bi55MMQWNkw1y7ahws4",
+    authDomain: "react-blog-app-8767f.firebaseapp.com",
+    projectId: "react-blog-app-8767f",
+    storageBucket: "react-blog-app-8767f.appspot.com",
+    messagingSenderId: "80591705151",
+    appId: "1:80591705151:web:fe2d518fca3d30a683d48a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
